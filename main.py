9304914d324f73208f52c20c8d53a1bc0365fcdc
ggEx86 <mxbalcewicz@gmail.com>
2020-05-27 17:01:03 +0200

    "Working OOP model, Game class and Player class added properly"

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pygame
-from pygame.locals import *
 
-pygame.init()
 walkRight = [pygame.image.load('Resources/R1.png'),
              pygame.image.load('Resources/R2.png'),
              pygame.image.load('Resources/R3.png'),
@@ -21,22 +19,37 @@ walkLeft = [pygame.image.load('Resources/L1.png'),
             pygame.image.load('Resources/L8.png'),
             pygame.image.load('Resources/L9.png')]
 bg = pygame.image.load('Resources/bg.jpg')
-character = pygame.image.load('Resources/standing.png')
+char = pygame.image.load('Resources/standing.png')
 clock = pygame.time.Clock()
-shoot_right = pygame.image.load('Resources/shoot_right.png')
-shoot_left = pygame.image.load('Resources/shoot_left.png')
 
 
-class Bullet(object):
-    def __init__(self, b_x, b_y):
-        self.b_x = b_x
-        self.b_y = b_y
-        self.b_im_r = pygame.image.load('Resources/bullet_r')
-        self.b_im_l = pygame.image.load('Resources/bullet_l')
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.keys = pygame.key.get_pressed()
+        self.run = True
+        self.win = pygame.display.set_mode((500, 480))
+        pygame.display.set_caption("Game")
 
-    def shoot_b(self, win):
-        pass
+    def gameLoop(self, _player):
+        self.run = True
+        while self.run:
+            clock.tick(27)
 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.run = False
+
+            self.keys = pygame.key.get_pressed()
+            _player.player_movement(self)
+
+            self.redrawGameWindow(_player)
+
+    def redrawGameWindow(self, _player):
+        self.win.blit(bg, (0, 0))
+        _player.draw(self.win)
+
+        pygame.display.update()
 
 
 class Player(object):
@@ -47,42 +60,30 @@ class Player(object):
         self.height = height
         self.vel = 5
         self.isJump = False
-        self.jumpCount = 10
         self.left = False
         self.right = False
         self.walkCount = 0
+        self.jumpCount = 10
 
     def draw(self, win):
         if self.walkCount + 1 >= 27:
             self.walkCount = 0
 
         if self.left:
-            if keys[pygame.K_LCTRL]:
-                self.shoot_animation(win)
-            else:
-                win.blit(walkLeft[self.walkCount // 3], (self.x, self.y))
-                self.walkCount += 1
+            win.blit(walkLeft[self.walkCount // 3], (self.x, self.y))
+            self.walkCount += 1
         elif self.right:
-            if keys[pygame.K_LCTRL]:
-                self.shoot_animation(win)
-            else:
-                win.blit(walkRight[self.walkCount // 3], (self.x, self.y))
-                self.walkCount += 1
+            win.blit(walkRight[self.walkCount // 3], (self.x, self.y))
+            self.walkCount += 1
         else:
-            win.blit(character, (self.x, self.y))
+            win.blit(char, (self.x, self.y))
 
-    def shoot_animation(self, win):
-        if self.right:
-            win.blit(shoot_right, (self.x, self.y))
-        if self.left:
-            win.blit(shoot_left, (self.x, self.y))
-
-    def player_movement(self):
-        if keys[pygame.K_LEFT] and self.x - self.vel > 0:  # left
+    def player_movement(self, _game):
+        if _game.keys[pygame.K_LEFT] and self.x > self.vel:
             self.x -= self.vel
             self.left = True
             self.right = False
-        elif keys[pygame.K_RIGHT] and self.x + self.width + self.vel < w_width:  # right
+        elif _game.keys[pygame.K_RIGHT] and self.x < 500 - self.width - self.vel:
             self.x += self.vel
             self.right = True
             self.left = False
@@ -92,41 +93,25 @@ class Player(object):
             self.walkCount = 0
 
         if not self.isJump:
-            if keys[pygame.K_SPACE]:
+            if _game.keys[pygame.K_SPACE]:
                 self.isJump = True
+                self.right = False
+                self.left = False
+                self.walkCount = 0
         else:
             if self.jumpCount >= -10:
-                self.y -= (self.jumpCount * abs(self.jumpCount)) * 0.5
+                neg = 1
+                if self.jumpCount < 0:
+                    neg = -1
+                self.y -= (self.jumpCount ** 2) * 0.5 * neg
                 self.jumpCount -= 1
             else:
-                self.jumpCount = 10
                 self.isJump = False
-
-def redrawGameWindow():
-    global walkCount
-    win.blit(bg, (0, 0))
-    char.draw(win)
-
-    pygame.display.update()
+                self.jumpCount = 10
 
 
 if __name__ == '__main__':
+    _game = Game()
+    _game.gameLoop(_player=Player(200, 410, 64, 64))
 
-    char = Player(300, 410, 64, 64)  # player object
-    size = w_width, w_height = 640, 480
-    win = pygame.display.set_mode(size)
-    pygame.display.set_caption('Game')
-
-    run = True
-    while run:
-        clock.tick(27)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-
-        keys = pygame.key.get_pressed()
-        char.player_movement()
-
-        redrawGameWindow()
     pygame.quit()
